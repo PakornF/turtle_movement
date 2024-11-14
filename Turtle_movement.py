00e6a4ebecd1bc484a6633c9fc42bf791a0a51e7
Pakorn Fudulyawajananont <pakorn.f@ku.th>
2024-11-14 23:40:22 +0700

    Second commit

--- Turtle_movement.py
+++ Turtle_movement.py
@@ -1,9 +1,47 @@
 import turtle
+state = 0
 
-def move_forward():
-     turtle.forward(100)
 
-turtle.onkey(move_forward, "Up")
+def move_up():
+    global state
+    if state != 1:
+        turtle.setheading(90)
+        state = 1
+    else:
+        turtle.forward(20)
+
+
+def move_right():
+    global state
+    if state != 2:
+        turtle.setheading(360)
+        state = 2
+    else:
+        turtle.forward(20)
+
+
+def move_left():
+    global state
+    if state != 3:
+        turtle.setheading(180)
+        state = 3
+    else:
+        turtle.forward(20)
+
+
+def move_down():
+    global state
+    if state != 4:
+        turtle.setheading(270)
+        state = 4
+    else:
+        turtle.forward(20)
+
+
+turtle.onkey(move_up, "Up")
+turtle.onkey(move_right, "Right")
+turtle.onkey(move_left, "Left")
+turtle.onkey(move_down, "Down")
 
 turtle.listen()
 turtle.done()
